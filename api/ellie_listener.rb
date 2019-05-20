8a632b5efb64f360eb7b69b4e127d7a1a0973d13
FLWallace105 <fwallace@fambrands.com>
2019-05-20 11:32:12 -0700

    changed api/ellie_listener.rb to have subscrption_skip less than 6th day of the month to skip

--- api/ellie_listener.rb
+++ api/ellie_listener.rb
@@ -380,7 +380,7 @@ class EllieListener < Sinatra::Base
     my_action = params['action']
     my_now = Date.current.day
     puts "Day of the month is #{my_now}"
-    if my_now < 50
+    if my_now < 6
       if my_action == "skip_month"
         #Add code to immediately skip the sub in DB only here
         local_sub_id = params['subscription_id']
