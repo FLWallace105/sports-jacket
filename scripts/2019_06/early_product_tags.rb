0546ca976342681e6c57a229d50bb44c7ef5d299
Floyd Wallace <floyd@ip-172-31-1-95.ec2.internal>
2019-06-18 10:27:32 -0700

    adding to temp master for git pull merge

---
+++ scripts/2019_06/early_product_tags.rb
@@ -0,0 +1,207 @@
+#!/bin/ruby
+# ELLIE LIVE
+require_relative '../../config/environment'
+
+month_start = Time.local(2019, 06)
+month_end = month_start.end_of_month
+early_tag = { active_start: nil, active_end: month_end }
+
+# Prepaid (current)
+prepaid_3 = 2209786298426
+prepaid_5 = 2209789771834
+prepaid_3_autorenew = 2243360030778
+prepaid_5_autorenew = 2243359604794
+# set prepaid tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3, tag: 'prepaid').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5, tag: 'prepaid').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3_autorenew, tag: 'prepaid').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5_autorenew, tag: 'prepaid').update(early_tag)
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prepaid_5_autorenew, tag: 'switchable').update(early_tag)
+
+
+# Prepaid (legacy)
+old_3_months = 23729012754
+vip_3_month = 9175678162
+old_3_months_autorenew = 10301516626
+# set prepaid tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months, tag: 'prepaid').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: vip_3_month, tag: 'prepaid').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months_autorenew, tag: 'prepaid').update(early_tag)
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: vip_3_month, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: vip_3_month, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: vip_3_month, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: old_3_months_autorenew, tag: 'switchable').update(early_tag)
+
+
+# June_collection: Good Intentions
+good_3 = 2405031280698
+good_5 = 2405031477306
+good_3_autorenew = 2418358190138
+good_5_autorenew = 2418451677242
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: good_5_autorenew, tag: 'switchable').update(early_tag)
+
+# June collection: Off the Track
+off_track_lap_3 = 2405032362042
+off_track_lap_5 = 2405032820794
+off_track_3_autorenew = 2418378244154
+off_track_5_autorenew = 2418380898362
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_lap_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_lap_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_lap_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_lap_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_lap_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_lap_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: off_track_5_autorenew, tag: 'switchable').update(early_tag)
+
+# June collection: Catch Up
+catch_up_3 = 2405033246778
+catch_up_5 = 2405033607226
+catch_up_3_autorenew = 2418473271354
+catch_up_5_autorenew = 2418479333434
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: catch_up_5_autorenew, tag: 'switchable').update(early_tag)
+
+# June collection: Dream Chaser
+dream_chaser_3 = 2405033902138
+dream_chaser_5 = 2405034393658
+dream_chaser_3_autorenew = 2419637649466
+dream_chaser_5_autorenew = 2419588890682
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: dream_chaser_5_autorenew, tag: 'switchable').update(early_tag)
+
+
+# June collection: Ready to Run
+ready_run_3 = 2387469467706
+ready_run_5 = 2372788551738
+ready_run_3_autorenew = 2419907854394
+ready_run_5_autorenew = 2418843779130
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: ready_run_5_autorenew, tag: 'switchable').update(early_tag)
+
+
+# bonus june_collection: Brooklyn
+brooklyn_3 = 2339789013050
+brooklyn_5 = 2339790127162
+brooklyn_3_autorenew = 2343841792058
+brooklyn_5_autorenew = 2343842807866
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: brooklyn_5_autorenew, tag: 'switchable').update(early_tag)
+
+
+# bonus june_collection: Prism Perfect
+prism_perfect_3 = 2339796320314
+prism_perfect_5 = 2339797532730
+prism_perfect_3_autorenew = 2342557843514
+prism_perfect_5_autorenew = 2342559973434
+# set current tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_3, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_5, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_3_autorenew, tag: 'current').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_5_autorenew, tag: 'current').update(early_tag)
+# set skippable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_3, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_5, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_3_autorenew, tag: 'skippable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_5_autorenew, tag: 'skippable').update(early_tag)
+# set switchable tags
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_3, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_5, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_3_autorenew, tag: 'switchable').update(early_tag)
+ProductTag.create_with(early_tag).find_or_create_by(product_id: prism_perfect_5_autorenew, tag: 'switchable').update(early_tag)
+
