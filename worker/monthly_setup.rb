640f7d80323a8c6d1699123e1188f734dcce9b4b
nevillealee <nevillealee@gmail.com>
2019-09-05 17:10:31 -0700

    error fix in auto config

--- worker/monthly_setup.rb
+++ worker/monthly_setup.rb
@@ -95,4 +95,4 @@ class MonthlySetup
       @conn.close
   end
 end
-COLLECTION_IDS = ProductTag.where("active_end = ?", "2019-10-01 06:59:59.999999").pluck(:product_id)
+COLLECTION_IDS = ProductTag.where("active_end = ?", "2019-09-30 23:59:59.999999").pluck(:product_id)
