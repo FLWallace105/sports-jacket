e0e0c9919c4e92490f8096b4cbc5f396a1dbe34e
FLWallace105 <fwallace@fambrands.com>
2020-03-02 15:17:37 -0800

    debug exit

--- worker/subscription_skip.rb
+++ worker/subscription_skip.rb
@@ -51,6 +51,7 @@ class SubscriptionSkip
       puts "now updated line item properties = #{my_temp_stuff}"
       stuff_to_recharge = { "properties" => my_temp_stuff }
       body = stuff_to_recharge.to_json
+      exit
 
       #my_update_sub = HTTParty.put("https://api.rechargeapps.com/subscriptions/#{my_sub_id}", :headers => 
       #recharge_change_header, :body => body, :timeout => 80)
