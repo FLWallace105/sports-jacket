d134d29b287dd6b4b0f4baeb92d989f2d56408cd
Jonathan Cabas <jcabas.ing@gmail.com>
2021-02-10 19:53:08 -0500

    Enables shopify setup from shopify_init file

--- lib/shopify.rb
+++ lib/shopify.rb
@@ -6,9 +6,6 @@ class Shopify
   def initialize(shop_url)
     puts shop_url.inspect
     ShopifyAPI::Base.site = shop_url.nil? ? default_url : shop_url
-    ShopifyAPI::Base.api_version = '2020-04'
-    ShopifyAPI::Base.timeout = 180
-    ShopifyAPI::GraphQL.initialize_clients
   end
 
   private
